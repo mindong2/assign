507e44d0c1b91ca79d48999c7e91ed5c7cbe662a
Dongmin Kim <qlsnaos12@naver.com>
2022-10-11 13:16:12 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue22.js App"/>
   </div>
 </template>
 
